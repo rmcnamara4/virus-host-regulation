9aa17d8e023edaea10027d4baf51960a4dc8062c
Ryan McNamara <RMcNamara@stowers.org>
2022-03-11 15:17:14 -0600

    Modify split_gff.sh file and add files produced by this script

--- Experiments/1-CalculateMosquitoOptimality/split_gff.sh
+++ Experiments/1-CalculateMosquitoOptimality/split_gff.sh
@@ -3,6 +3,8 @@
 # script to separate exons and introns in the gff file 
 # create exon gff file 
 cat data/AaloF1.EnsGen_50.gff | grep exon | grep protein_coding > AaloF1.exons.gff
+mv AaloF1.exons.gff data/AaloF1.exons.gff
 
 # create intron gff file 
 cat data/AaloF1.EnsGen_50.gff | grep intron > AaloF1.introns.gff 
+mv AaloF1.introns.gff data/AaloF1.introns.gff
