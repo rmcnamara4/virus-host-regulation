55d8428ea461010ee1a8e0ce36803d554758bc1f
Ryan McNamara <RMcNamara@stowers.org>
2022-03-11 15:01:34 -0600

    Add bash file to get all required files

---
+++ Experiments/1-CalculateMosquitoOptimality/get_files.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# script to gather all of the required data for the analysis 
+# make dir to put the data 
+mkdir -p data
+
+# get Aedes albopictus gff file 
+wget https://webfs/n/analysis/indexes/AaloF1/annotation/EnsGen_50/gtfs/AaloF1.EnsGen_50.gff
+mv AaloF1.EnsGen_50.gff data/AaloF1.EnsGen_50.gff
+
+# get dmso aligned bam file and bai file 
+wget https://webfs/n/core/Bioinformatics/secondary/Bazzini/lc2196/MOLNG-3268/s_dmso/s_dmso.Aligned.sortedByCoord.out.bam 
+mv s_dmso.Aligned.sortedByCoord.out.bam data/s_dmso.Aligned.sortedByCoord.out.bam
+
+wget https://webfs/n/core/Bioinformatics/secondary/Bazzini/lc2196/MOLNG-3268/s_dmso/s_dmso.Aligned.sortedByCoord.out.bam.bai
+mv s_dmso.Aligned.sortedByCoord.out.bam.bai data/s_dmso.Aligned.sortedByCoord.out.bam.bai
+
+# get drb aligned bam file and bai file 
+wget https://webfs/n/core/Bioinformatics/secondary/Bazzini/lc2196/MOLNG-3268/s_drb_1/s_drb_1.Aligned.sortedByCoord.out.bam
+mv s_drb_1.Aligned.sortedByCoord.out.bam data/s_drb_1.Aligned.sortedByCoord.out.bam
+
+wget https://webfs/n/core/Bioinformatics/secondary/Bazzini/lc2196/MOLNG-3268/s_drb_1/s_drb_1.Aligned.sortedByCoord.out.bam.bai 
+mv s_drb_1.Aligned.sortedByCoord.out.bam.bai data/s_drb_1.Aligned.sortedByCoord.out.bam.bai 
+
+# get flavopiridol bam file and bai file 
+wget https://webfs/n/core/Bioinformatics/secondary/Bazzini/lc2196/MOLNG-3268/s_flavopiridol_2/s_flavopiridol_2.Aligned.sortedByCoord.out.bam 
+mv s_flavopiridol_2.Aligned.sortedByCoord.out.bam data/s_flavopiridol_2.Aligned.sortedByCoord.out.bam 
+
+wget https://webfs/n/core/Bioinformatics/secondary/Bazzini/lc2196/MOLNG-3268/s_flavopiridol_2/s_flavopiridol_2.Aligned.sortedByCoord.out.bam.bai 
+mv s_flavopiridol_2.Aligned.sortedByCoord.out.bam.bai data/s_flavopiridol_2.Aligned.sortedByCoord.out.bam.bai 
+
+# get triptolide bam file and bai file 
+wget https://webfs/n/core/Bioinformatics/secondary/Bazzini/lc2196/MOLNG-3268/s_triptolide_3/s_triptolide_3.Aligned.sortedByCoord.out.bam   
+mv s_triptolide_3.Aligned.sortedByCoord.out.bam data/s_triptolide_3.Aligned.sortedByCoord.out.bam   
+
+wget https://webfs/n/core/Bioinformatics/secondary/Bazzini/lc2196/MOLNG-3268/s_triptolide_3/s_triptolide_3.Aligned.sortedByCoord.out.bam.bai   
+mv s_triptolide_3.Aligned.sortedByCoord.out.bam.bai data/s_triptolide_3.Aligned.sortedByCoord.out.bam.bai   
