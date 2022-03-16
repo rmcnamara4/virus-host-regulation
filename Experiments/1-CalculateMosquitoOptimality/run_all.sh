3f3f1259e62791a63b640c595104be141e43c9df
Ryan McNamara <RMcNamara@stowers.org>
2022-03-16 10:45:47 -0500

    Add run_all.sh file to run all of the bash scripts in order

---
+++ Experiments/1-CalculateMosquitoOptimality/run_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+# script to run all of the bash scripts in order 
+# run script to collect required files 
+bash get_files.sh
+
+# run script to split gff file 
+bash split_gff.sh
+
+# run script to map the introns 
+bash map_introns.sh
