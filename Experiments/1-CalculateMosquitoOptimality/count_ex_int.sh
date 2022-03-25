4b421f15dc79c8d3343ffbd52db75de6a7e68bbf
Ryan McNamara <RMcNamara@stowers.org>
2022-03-25 16:00:35 -0500

    Modify count_ex_int.sh file

--- Experiments/1-CalculateMosquitoOptimality/count_ex_int.sh
+++ Experiments/1-CalculateMosquitoOptimality/count_ex_int.sh
@@ -2,18 +2,18 @@
 
 # script to count the intron and exon reads  
 # map the dmso sample
-htseq-count -m intersection-strict -f bam -t exon -s no -i Parent data/bam/s_dmso.Aligned.sortedByCoord.out.bam data/AaloF1.exons.gff > data/counts/s_dmso.exons.counts.txt
-htseq-count -m union -f bam -t intron -s no -i Parent data/bam/s_dmso.Aligned.sortedByCoord.out.bam data/AaloF1.introns.gff > data/counts/s_dmso.introns.counts.txt
+htseq-count -m intersection-strict -f bam -t exon -s no -i Parent data/bam/s_dmso.Aligned.sortedByCoord.out.bam data/AaloF1.exons.gff > data/counts/s_dmso.exons.counts.me.txt
+htseq-count -m union -f bam -t intron -s no -i Parent data/bam/s_dmso.Aligned.sortedByCoord.out.bam data/AaloF1.introns.gff > data/counts/s_dmso.introns.counts.me.txt
 
 # map the drb sample
-htseq-count -m intersection-strict -f bam -t exon -s no -i Parent data/bam/s_drb_1.Aligned.sortedByCoord.out.bam data/AaloF1.exons.gff > data/counts/s_drb_1.exons.counts.txt
-htseq-count -m union -f bam -t intron -s no -i Parent data/bam/s_drb_1.Aligned.sortedByCoord.out.bam data/AaloF1.introns.gff > data/counts/s_drb_1.introns.counts.txt
+htseq-count -m intersection-strict -f bam -t exon -s no -i Parent data/bam/s_drb_1.Aligned.sortedByCoord.out.bam data/AaloF1.exons.gff > data/counts/s_drb_1.exons.counts.me.txt
+htseq-count -m union -f bam -t intron -s no -i Parent data/bam/s_drb_1.Aligned.sortedByCoord.out.bam data/AaloF1.introns.gff > data/counts/s_drb_1.introns.counts.me.txt
 
 # map the flavopiridol sample 
-htseq-count -m intersection-strict -f bam -t exon -s no -i Parent data/bam/s_flavopiridol_2.Aligned.sortedByCoord.out.bam data/AaloF1.exons.gff > data/counts/s_flavopiridol_2.exons.counts.txt
-htseq-count -m union -f bam -t intron -s no -i Parent data/bam/s_flavopiridol_2.Aligned.sortedByCoord.out.bam data/AaloF1.introns.gff > data/counts/s_flavopiridol_2.introns.counts.txt
+htseq-count -m intersection-strict -f bam -t exon -s no -i Parent data/bam/s_flavopiridol_2.Aligned.sortedByCoord.out.bam data/AaloF1.exons.gff > data/counts/s_flavopiridol_2.exons.counts.me.txt
+htseq-count -m union -f bam -t intron -s no -i Parent data/bam/s_flavopiridol_2.Aligned.sortedByCoord.out.bam data/AaloF1.introns.gff > data/counts/s_flavopiridol_2.introns.counts.me.txt
 
 # map the triptolide sample 
-htseq-count -m intersection-strict -f bam -t exon -s no -i Parent data/bam/s_triptolide_3.Aligned.sortedByCoord.out.bam data/AaloF1.exons.gff > data/counts/s_triptolide_3.exons.counts.txt
-htseq-count -m union -f bam -t intron -s no -i Parent data/bam/s_triptolide_3.Aligned.sortedByCoord.out.bam data/AaloF1.introns.gff > data/counts/s_triptolide_3.introns.counts.txt
+htseq-count -m intersection-strict -f bam -t exon -s no -i Parent data/bam/s_triptolide_3.Aligned.sortedByCoord.out.bam data/AaloF1.exons.gff > data/counts/s_triptolide_3.exons.counts.me.txt
+htseq-count -m union -f bam -t intron -s no -i Parent data/bam/s_triptolide_3.Aligned.sortedByCoord.out.bam data/AaloF1.introns.gff > data/counts/s_triptolide_3.introns.counts.me.txt
 
