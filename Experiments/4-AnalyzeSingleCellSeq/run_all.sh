133cb18157a62a28aacbe663a32cfc2a158b44f1
Ryan McNamara <RMcNamara@stowers.org>
2022-04-15 16:11:47 -0500

    Modify the run_all.sh script to create required directories

--- Experiments/4-AnalyzeSingleCellSeq/run_all.sh
+++ Experiments/4-AnalyzeSingleCellSeq/run_all.sh
@@ -7,3 +7,7 @@ bash download_data.sh
 # run script to calculate CPMs
 mkdir -p data/cpms
 Rscript calculate_cpms.R
+
+# make directories that are needed to save files in the differential_expression_dengue.Rmd analysis
+mkdir -p data/genes
+mkdir -p data/edgeR
