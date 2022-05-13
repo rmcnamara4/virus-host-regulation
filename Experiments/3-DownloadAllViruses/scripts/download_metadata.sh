0dd5314ca260bc3b209545ac4552b921d3591901
Ryan McNamara <RMcNamara@stowers.org>
2022-05-13 17:41:24 -0500

    Modify download_metadata.sh and resulting metadata.txt file

--- Experiments/3-DownloadAllViruses/scripts/download_metadata.sh
+++ Experiments/3-DownloadAllViruses/scripts/download_metadata.sh
@@ -11,22 +11,15 @@ mv assembly_summary.txt ./complete_assembly.txt
 
 # download and unzip the gbff files from RefSeq to get other virus information
 wget https://ftp.ncbi.nlm.nih.gov/refseq/release/viral/viral.1.genomic.gbff.gz
-gunzip viral.1.genomic.gbff.gz
-
 wget https://ftp.ncbi.nlm.nih.gov/refseq/release/viral/viral.2.genomic.gbff.gz
-gunzip viral.2.genomic.gbff.gz
-
 wget https://ftp.ncbi.nlm.nih.gov/refseq/release/viral/viral.3.genomic.gbff.gz
-gunzip viral.3.genomic.gbff.gz
-
 wget https://ftp.ncbi.nlm.nih.gov/refseq/release/viral/viral.4.genomic.gbff.gz
-gunzip viral.4.genomic.gbff.gz
 
 # concatenate the gbff files to one file
-cat *.gbff > viruses.gbff
+cat *.gbff.gz > viruses.gbff.gz
 
 # remove the individual files
-rm viral.1.genomic.gbff
-rm viral.2.genomic.gbff
-rm viral.3.genomic.gbff
-rm viral.4.genomic.gbff
+rm viral.1.genomic.gbff.gz
+rm viral.2.genomic.gbff.gz
+rm viral.3.genomic.gbff.gz
+rm viral.4.genomic.gbff.gz
