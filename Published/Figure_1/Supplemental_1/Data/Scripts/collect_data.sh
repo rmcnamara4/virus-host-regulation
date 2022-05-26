895df023f27d812f1e084edd61ae83b8a113ef9a
Ryan McNamara <RMcNamara@stowers.org>
2022-05-26 15:22:59 -0500

    Add bash script to copy data from the main data to supplemental data and add run_all.sh script

---
+++ Published/Figure_1/Supplemental_1/Data/Scripts/collect_data.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+
+# script to copy data that is used in main figure to the folder for the supplemental figure 
+# copy data 
+cp ../../../Main/Data/viral_rscu_fc_human.csv ..
+cp ../../../Main/Data/human_endo_csc.csv ..
+
+
---
+++ Published/Figure_1/Supplemental_1/Data/Scripts/run_all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+
+# script to run all of the data creation/collection 
+# run collection script 
+
+bash collect_data.sh
