c7bed1264b0f3d3cf5a272c2c23474023c40460c
Ryan McNamara <RMcNamara@stowers.org>
2022-06-01 15:56:32 -0500

    Modify run_all.sh script

--- Published/Figure_2/Supplemental_1/Data/Scripts/run_all.sh
+++ Published/Figure_2/Supplemental_1/Data/Scripts/run_all.sh
@@ -4,3 +4,5 @@
 # run script to gather optimality data 
 bash gather_optimality_data.sh
 
+# run script to create mosquito codon frequency ratio table 
+Rscript create_mosquito_codon_frequency_ratio.R
