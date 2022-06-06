ca4a2ef2d062c488970060445a68e3ce804871f0
Ryan McNamara <RMcNamara@stowers.org>
2022-06-06 11:50:43 -0500

    Add Rscript to collect the viral RSCU fold change human file from Figure_1 and add run command to run_all.sh

--- Published/Figure_3/Main/Data/Scripts/run_all.sh
+++ Published/Figure_3/Main/Data/Scripts/run_all.sh
@@ -6,3 +6,6 @@ bash collect_opt.sh
 
 # run script to create viral RSCU fold change mosquito table
 Rscript create_viral_rscu_fc_mosquito.R
+
+# run script to collect viral RSCU fold change human table 
+bash collect_viral_rscu_fc_human.sh
