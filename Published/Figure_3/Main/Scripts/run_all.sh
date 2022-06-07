22df2b3b7effff1ef4630c973e25b82f08b85760
Ryan McNamara <RMcNamara@stowers.org>
2022-06-07 14:22:10 -0500

    Modify run_all.sh

---
+++ Published/Figure_3/Main/Scripts/run_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# script to run all of the data creation scripts
+# run script to collect optimality data
+bash collect_opt.sh
+
+# run script to create viral RSCU fold change mosquito table
+Rscript create_viral_rscu_fc_mosquito.R
+
+# run script to collect viral RSCU fold change human table
+bash collect_viral_rscu_fc_human.sh
