7b79aa5118d98ac7f968a8372bc1a99d7a255ade
Ryan McNamara <RMcNamara@stowers.org>
2022-07-27 19:51:30 -0500

    Add collect_data.sh and run_all.sh and add datasets from running the scripts

---
+++ Published/Figure_6/Supplemental_1/Data/Scripts/collect_data.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# script to collect required data files
+# copy codon optimalities
+cp ../../../Main/Data/codon_optimalities.csv ..
+
+# copy mutation data sets
+cp ../../../Main/Data/syn_mutations_fitness_class_percent_per_species_p9.csv ..
+cp ../../../Main/Data/syn_mutations_mrf_per_rep_p9.csv ..
+cp ../../../Main/Data/syn_mutations_mrf_per_species_p9.csv ..
+cp ../../../Main/Data/syn_mutations_per_species_p9.csv ..
