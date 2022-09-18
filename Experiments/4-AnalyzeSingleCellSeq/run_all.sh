26dc0741ea6abf94aa24f3bd80c0a0d4c32b0b12
Ryan McNamara <RMcNamara@stowers.org>
2022-09-18 16:47:03 -0500

    Modify run_all.sh

--- Experiments/4-AnalyzeSingleCellSeq/run_all.sh
+++ Experiments/4-AnalyzeSingleCellSeq/run_all.sh
@@ -8,6 +8,10 @@ bash download_data.sh
 mkdir -p data/cpms
 Rscript calculate_cpms.R
 
+# run script to get go terms 
+mkdir -p data/go_terms
+Rscript get_human_go_terms.R
+
 # make directories that are needed to save files in the differential_expression_dengue.Rmd analysis
 mkdir -p data/genes
 mkdir -p data/edgeR
